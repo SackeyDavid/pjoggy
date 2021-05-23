d98067bae74848480f8a7e93a5ccee2b977ce988
David <kofiahendev@gmail.com>
2021-05-23 15:49:11 +0000

    4r2 commit

--- src/app/pages/user-auth/signup-more-info/signup-more-info.component.ts
+++ src/app/pages/user-auth/signup-more-info/signup-more-info.component.ts
@@ -16,11 +16,21 @@ export class SignupMoreInfoComponent implements OnInit {
   errorMsgs: any = {};
   showPrompt: Boolean = false;
 
+  token_from_url: string = '';
+  token: string = '';
+
   public registerForm: FormGroup = new FormGroup({});
 
   constructor(private auth: UserAuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.token_from_url = this.router.url
+    var ind1 = this.token_from_url.indexOf('/');
+    var ind2 = this.token_from_url.indexOf('=', ind1 + 1);
+    this.token = this.token_from_url.substring(ind2 + 1);
+    sessionStorage.setItem('registration_id', this.token);
+    // var ind4 = this.token_from_url.indexOf('-', ind3 + 1);
+    
     this.registerForm = new FormGroup({
       firstname: new FormControl('', Validators.required),
       lastname: new FormControl('', Validators.required),
